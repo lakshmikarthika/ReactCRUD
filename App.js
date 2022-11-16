b3deba590a4cd584f431b5b2d3fe5261e25aa7a6
karthika <49395313+lakshmikarthika@users.noreply.github.com>
2022-11-16 15:53:46 +0530

    First commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Create from './components/Create';
+import Read from './components/Read';
+
+function App() {
+  return (
+    <div className="App">
+      <Create />
+      <Read />
+    </div>
+  );
+}
+
+export default App;
