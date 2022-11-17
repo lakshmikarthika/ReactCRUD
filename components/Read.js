750d00b93f9a509fe60abe01d06ebbd322e67ad8
karthika <49395313+lakshmikarthika@users.noreply.github.com>
2022-11-17 10:38:07 +0530

    Update Read.js

--- components/Read.js
+++ components/Read.js
@@ -1,13 +1,29 @@
 import React,{useEffect,useState} from "react";
 import axios from "axios";
 import { Label, Table, Button } from "semantic-ui-react";
+import {Link} from 'react-router-dom'
 
 function Read() {
     const [apiData,setApiData] = useState([])
     useEffect(()=> {
+        getData();
+    })
+    const setData = (id,fn,ln) => {
+        localStorage.setItem('Id',id);
+        localStorage.setItem('fname',fn);
+        localStorage.setItem('lname',ln);
+    }
+    const getData = () => {
         axios.get('https://63747e9548dfab73a4e0ef1b.mockapi.io/Crud')
         .then((res)=> setApiData(res.data));
-    })
+    }
+    const deleteData = (id) => {
+        axios.delete(`https://63747e9548dfab73a4e0ef1b.mockapi.io/Crud/${id}`)
+        .then(()=> {
+            getData();
+        })
+
+    }
   return (
     <div>
       <Table celled>
@@ -28,8 +44,8 @@ function Read() {
                 </Table.Cell>
                 <Table.Cell>{data.fname}</Table.Cell>
                 <Table.Cell>{data.lname}</Table.Cell>
-                <Table.Cell><Button color="green">Update</Button></Table.Cell>
-                <Table.Cell><Button color="red">Delete</Button></Table.Cell>
+                <Table.Cell><Link to="/update"><Button color="green" onClick={()=> setData(data.id,data.fname,data.lname)}>Update</Button></Link></Table.Cell>
+                <Table.Cell><Button color="red" onClick={()=> deleteData(data.id)}>Delete</Button></Table.Cell>
               </Table.Row>
             ))}
         </Table.Body>
