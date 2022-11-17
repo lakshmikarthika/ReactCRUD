8e9032fbcf5ad3dc54516138eb4db0eab40fcb05
karthika <49395313+lakshmikarthika@users.noreply.github.com>
2022-11-17 10:37:40 +0530

    Update Create.js

--- components/Create.js
+++ components/Create.js
@@ -1,15 +1,18 @@
 import React, { useState } from 'react'
 import { Button, Form } from 'semantic-ui-react'
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const Create = () => {
     const [fname, setFname] = useState('');
     const [lname, setLname] = useState('');
+    const navigate = useNavigate();
     const sendData = () => {
         axios.post('https://63747e9548dfab73a4e0ef1b.mockapi.io/Crud',{
             fname,
             lname
         })
+        navigate('/read')
     }
     return (
         <>
